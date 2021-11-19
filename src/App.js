8362ca30249be9adeb7c367918f7e685b02e08bb
kingbaddo <wizzyblaq94@gmail.com>
2021-11-20 00:41:01 +0100

    navigation and hero

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-         <h1>Hello Tailwind CSS</h1>
-         <div className="p-6 max-w-sm mx-auto bg-white roundedd-xl shadow-md flex items-center">
-           <div className="flex-shrink-0">
-
-           </div>
-           <div className="text-xl font-medium text-black">ChitChat</div>
-         </div>
+         <Navbar />
+         <Main />
+         
     </div>
   );
 }
